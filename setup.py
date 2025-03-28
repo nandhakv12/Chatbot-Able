26b8fc5fc405906505bfb4cedf4eea9377db5ea2
nandhakv12 <91311782+nandhakv12@users.noreply.github.com>
2025-03-28 14:38:50 -0400

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Chatbot-Able',
+    version= '1.0.0',
+    author= 'Nandhakumar',
+    author_email= 'nandhakv12@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
