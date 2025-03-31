9c61da0a8321c550fce1cf4184751d654cd35164
nandhakv12 <91311782+nandhakv12@users.noreply.github.com>
2025-03-31 10:41:43 -0400

    deployement-changes

--- app.py
+++ app.py
@@ -71,4 +71,6 @@ def chat():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port= 5001, debug= True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
